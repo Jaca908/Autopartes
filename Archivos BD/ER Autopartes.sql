2e2939a4484ac598fd7d01854da063d1ab0a2f06
Errol28Rose <errolmagodeoz@gmail.com>
2019-12-09 14:41:06 -0600

    adicion de Diagrama y Script de BD

---
+++ Archivos BD/ER Autopartes.sql
@@ -0,0 +1,81 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS `modelo`;
+DROP TABLE IF EXISTS `generacion`;
+DROP TABLE IF EXISTS `grupo`;
+DROP TABLE IF EXISTS `subgrupo`;
+DROP TABLE IF EXISTS `repuesto`;
+DROP TABLE IF EXISTS `marca_repuesto`;
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE `modelo` (
+    `Codigo` VARCHAR(5) NOT NULL,
+    `Modelo` VARCHAR(50) NOT NULL,
+    `Estado` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`Codigo`)
+);
+
+CREATE TABLE `generacion` (
+    `Codigo` VARCHAR(3) NOT NULL,
+    `FK_Modelo` VARCHAR(5) NOT NULL,
+    `Generacion` VARCHAR(50) NOT NULL,
+    `Ano` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`Codigo`)
+);
+
+CREATE TABLE `grupo` (
+    `Codigo` VARCHAR(3) NOT NULL,
+    `Grupo` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`Codigo`)
+);
+
+CREATE TABLE `subgrupo` (
+    `Codigo` VARCHAR(3) NOT NULL,
+    `FK_Grupo` VARCHAR(3) NOT NULL,
+    `Subgrupo` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`Codigo`)
+);
+
+CREATE TABLE `repuesto` (
+    `FK_modelo` VARCHAR(5) NOT NULL,
+    `FK_generacion` VARCHAR(3) NOT NULL,
+    `FK_grupo` VARCHAR(3) NOT NULL,
+    `FK_subgrupo` VARCHAR(3) NOT NULL,
+    `Codigo` CHAR(3) NOT NULL,
+    `FK_marca_repuesto` INT NOT NULL,
+    `CodigoMarca` VARCHAR(100),
+    `CodigoAlterno` VARCHAR(100),
+    `RepuestoFinal` VARCHAR(100) NOT NULL,
+    `Peso` DECIMAL(10,2) NOT NULL,
+    `Dimension` VARCHAR(50) NOT NULL,
+    `Medida` VARCHAR(50) NOT NULL,
+    `Automatico` BOOLEAN NOT NULL,
+    `Manual` BOOLEAN NOT NULL,
+    `4X2` BOOLEAN NOT NULL,
+    `4X4` BOOLEAN NOT NULL,
+    `Gasolina` BOOLEAN NOT NULL,
+    `Dielsel` BOOLEAN NOT NULL,
+    `Electrico` BOOLEAN NOT NULL,
+    `Hibrido` BOOLEAN NOT NULL,
+    `Espacio1` VARCHAR(100),
+    `Espacio2` VARCHAR(100),
+    `Espacio3` VARCHAR(100),
+    `PrecioCosto` DECIMAL(10,2) NOT NULL,
+    `PrecioVenta` DECIMAL(10,2) NOT NULL,
+    `Utilidad` DECIMAL(10,2) NOT NULL,
+    `IVA` DECIMAL(5) NOT NULL,
+    PRIMARY KEY (`FK_modelo`, `FK_generacion`, `FK_grupo`, `FK_subgrupo`, `Codigo`)
+);
+
+CREATE TABLE `marca_repuesto` (
+    `Codigo` INT NOT NULL,
+    `MarcaRepuesto` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`Codigo`)
+);
+
+ALTER TABLE `generacion` ADD FOREIGN KEY (`FK_Modelo`) REFERENCES `modelo`(`Codigo`);
+ALTER TABLE `subgrupo` ADD FOREIGN KEY (`FK_Grupo`) REFERENCES `grupo`(`Codigo`);
+ALTER TABLE `repuesto` ADD FOREIGN KEY (`FK_modelo`) REFERENCES `modelo`(`Codigo`);
+ALTER TABLE `repuesto` ADD FOREIGN KEY (`FK_generacion`) REFERENCES `generacion`(`Codigo`);
+ALTER TABLE `repuesto` ADD FOREIGN KEY (`FK_grupo`) REFERENCES `grupo`(`Codigo`);
+ALTER TABLE `repuesto` ADD FOREIGN KEY (`FK_subgrupo`) REFERENCES `subgrupo`(`Codigo`);
+ALTER TABLE `repuesto` ADD FOREIGN KEY (`FK_marca_repuesto`) REFERENCES `marca_repuesto`(`Codigo`);
