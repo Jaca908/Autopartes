085ba55865165ac06e4dd251e37987f800aadab2
Errol28Rose <errolmagodeoz@gmail.com>
2019-12-09 14:56:54 -0600

    Cambios en Logica

--- Logica/Generacion.php
+++ Logica/Generacion.php
@@ -1,3 +1,151 @@
 <?php
+session_start();
+ 
+require ("../Conexion/Conexion.php");
+
+
+//Comprbar si el usuario presiono el boton de EnviarGeneracion
+
+if(isset($_POST["btnEnviar"]))/*Para guardar o modificar*/
+{
+	GuardarOModificar();
+}
+else if(isset($_POST['MostrarDatos']))/*Para consultar y llenar los campos*/
+{
+	Consultar();
+}
+
+function GuardarOModificar()
+{
+	$GuardarModificar=$_POST["GuardarModificar"];
+	
+	$Respuesta='';
+	$GuarMod='';
+
+	#ver si es guardar o Modificar
+	
+	if($GuardarModificar=="Guardar")
+	{
+		//Guardar
+
+		$Codigo=$_POST["Codigo"];
+		$Genaracion=$_POST["Genaracion"];
+		//$FK_Usuario=$_SESSION['IDUsuario'];
+		
+		$Conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+		if ($Conexion->connect_error) 
+		{
+			die("Connection failed: " . $Conexion->connect_error);
+		} 
+
+		$sql = "SELECT Codigo,Genaracion FROM genaracion WHERE Codigo = '$Codigo'";
+							
+		$result = $Conexion->query($sql);
+
+		if ($result->num_rows > 0) 
+		{
+		$Respuesta = "Ya existe un genaración con ese código";
+		$GuarMod='Error'; 
+		}
+		else # si no existe verificar que no haya otro grupo con el mismo nombre
+		{		
+			$sql = "SELECT Codigo,Genaracion FROM genaración WHERE Genaracion like '$Genaracion'";
+							
+			$result = $Conexion->query($sql);
+
+			if ($result->num_rows > 0) 
+			{
+				$Respuesta = "Ya existe una genaración con ese nombre"; 
+			}
+			else
+			{
+				//sanitize el sql
+				$sql = "INSERT INTO genaracion(Codigo,Genaracion)values('$Codigo','$Genaracion');";
+								
+				if($Conexion->query($sql) === TRUE) 
+				{   
+				  $Respuesta = "Genaración guardada exitosamente";
+				  $GuarMod='Guardo'; 			  
+				} 
+				else 
+				{
+				  $Respuesta = "Error al guardar la genaración";
+				  $GuarMod='Error';
+				}
+			}		
+		}		
+	}			
+	else if($GuardarModificar=="Modificar")
+	{
+		//modificar
+		
+		$Codigo=$_POST["Codigo"];
+		$Genaracion=$_POST["Genaracion"];
+		//$FK_Usuario=$_SESSION['IDUsuario'];
+		
+		$Conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+		if ($Conexion->connect_error) 
+		{
+		 	die("Connection failed: " . $Conexion->connect_error);
+		} 
+			//sanitize el sql
+			$sql = "UPDATE genaracion SET Genaracion='$Genaracion' where Codigo='$Codigo'";
+							
+			if($Conexion->query($sql) === TRUE) 
+			{   
+			  $Respuesta = "Genaración modificada exitosamente";
+			  $GuarMod = 'Modifico'; 			  
+			} 
+			else 
+			{
+				 $Respuesta = "Error al modificar la genaración";
+				 $GuarMod='Error';
+			}		
+	}
+	
+	$users_arr[] = array( 
+                            "Respuesta"=>$Respuesta,
+                            "GuarMod"=>$GuarMod,
+                       );
+
+      // encoding array to json format
+      echo json_encode($users_arr);
+      exit;
+}
+
+function Consultar()
+{
+	$Codigo=$_POST['Codigo'];
+	//$FK_Usuario=$_SESSION['IDUsuario'];
+
+	$Conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+	if ($Conexion->connect_error) 
+	{
+		die("Connection failed: " . $Conexion->connect_error);
+	} 
+
+	$sql = "SELECT Codigo,Generacion FROM generacion WHERE Codigo = '$Codigo'";
+						
+	$result = $Conexion->query($sql);
+
+	if ($result->num_rows > 0) 
+	{
+		$row = $result->fetch_assoc();
+
+		$Codigo= $row["Codigo"];
+		$Generacion=$row["Generacion"];
+	}
+
+	$users_arr[] = array( 
+                         "Codigo"=>$Codigo,"Generacion"=>$Generacion,
+                     );
+
+    // encoding array to json format
+    echo json_encode($users_arr);
+    exit; 
+}
 
 ?>
