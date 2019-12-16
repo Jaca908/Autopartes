55cad703b0387faf85c902ab48fb7894149ea1ea
Errol28Rose <errolmagodeoz@gmail.com>
2019-12-16 11:14:32 -0600

    Formularios en una pagina listos a espera de Marca de Repuesto Menu cambiado para que solo tenga 2 opciones: para ingresar y ver

--- forms/formingresarmodificardatos.php
+++ forms/formingresarmodificardatos.php
@@ -168,6 +168,66 @@
     </div>
 </div>
 
+<div id="DIVSubgrupo" class="form-inline">
+<legend>Ingrese el Subgrupo</legend>
+
+<label for="fname">Código<br>
+<input type="text" maxlength="3" id="CodigoSubgrupo" name="txtCodigo" placeholder="Código"/>
+</label>
+
+<label for="Grupo">Grupo<br>
+        <select id="cmbGrupo" class="select" name="Grupo">
+          <option value="" selected="selected"></option>
+        <?php
+          //Cargar Combobox de Modelos
+          
+          include '../Conexion/Conexion.php';
+          
+          $Conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+          if ($Conexion->connect_error) 
+          {
+            die("Connection failed: " . $Conexion->connect_error);
+          } 
+          
+          $sql = "SELECT Codigo,Grupo FROM grupo";           
+          $result = $Conexion->query($sql);
+        ?>
+        <?php while($ri =  mysqli_fetch_array($result))
+              {
+              echo "<option value=".$ri['Codigo'].">".$ri['Grupo']."</option>";
+              }
+        ?>
+        </select>
+    </label>
+
+    <label for="lname">Subgrupo<br>
+    <input type="text" maxlength="100" id="Subgrupo" name="txtSubgrupo" placeholder="Subgrupo"/>
+</label>
+
+<div id="divenviar">
+<input type="button" id="enviarSubgrupo" class="btnenviar" value="Enviar" onclick="EnviarSubgrupo()"></div>
+
+<!--Modal de mensajes-->
+<div class="modal fade" id="ModalMSJSubgrupo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+          <h4 class="modal-title" style="font-weight: bold; color:black;" id="exampleModalLabel">Subgrupo</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          </div>
+          <div class="modal-body" style="color:black;" id="MSJSubgrupo">
+          </div>
+          <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+        </div>
+    </div>
+</div>
+
 
 
     </div>   
@@ -280,7 +340,33 @@ var Form=sessionStorage.getItem("Form");
 	}
 	else if(Form=='Subgrupo')
 	{
-		
+		document.getElementById('CodigoSubgrupo').readOnly = true;
+
+    $.ajax({
+            url: '../Logica/Subgrupo.php',
+            type: 'post',
+            data: 
+            {
+              MostrarDatos:'MostrarDatos',
+              Codigo:Codigo
+            },
+            dataType: 'json',
+            success:function(response){
+              
+                var len = response.length;
+
+                if(len > 0)
+                {
+                  document.getElementById('CodigoSubgrupo').value = response[0]['Codigo'];
+                  document.getElementById('Subgrupo').value = response[0]['Subgrupo'];
+                  document.getElementById('cmbGrupo').value = response[0]['Grupo'];   
+                  
+        		}
+              
+            }
+        });
+
+        return false;
 	}
   }
   else
@@ -299,7 +385,7 @@ var Form=sessionStorage.getItem("Form");
 	}
 	else if(Form=='Subgrupo')
     {
-		
+		document.getElementById('CodigoSubgrupo').readOnly = false;
 	}
   }
 
@@ -544,6 +630,85 @@ $('#ModalMSJGrupo').on('hide.bs.modal', function (e) {
 	else if(GuarMod =='Modifico')
 	{
 		window.open('vergrupos.php', '_self');	
+	}
+});
+	
+</script>
+
+<script>
+	
+function EnviarSubgrupo()
+{//Funcion para Guardar o Modificar una generacion
+
+	if(document.getElementById('CodigoSubgrupo').value=='')
+	{
+		$("#MSJSubgrupo").html('Error: Ingrese un código de subgrupo');
+    	$("#ModalMSJSubgrupo").modal("show");	
+	}
+	else if(document.getElementById('cmbGrupo').value=='')
+	{
+		$("#MSJSubgrupo").html('Error: Seleccione un grupo');
+    	$("#ModalMSJSubgrupo").modal("show");	
+	}
+	else if(document.getElementById('Subgrupo').value=='')
+	{
+		$("#MSJSubgrupo").html('Error: Ingrese un subgrupo');
+    	$("#ModalMSJSubgrupo").modal("show");	
+	}
+	else
+	{  
+		$.ajax({
+          url: '../Logica/Subgrupo.php',
+          type: 'post',
+          data: 
+          {
+             btnEnviar:"Enviar",
+             GuardarModificar:($('#CodigoSubgrupo').is('[readonly]'))?"Modificar":"Guardar", 
+             Codigo:document.getElementById('CodigoSubgrupo').value,
+             Grupo:document.getElementById('cmbGrupo').value,
+             Subgrupo:document.getElementById('Subgrupo').value,             
+          },
+          dataType: 'json',
+          success:function(response){
+              
+              var len = response.length;
+
+              if(len > 0){
+                
+                var Respuesta=response[0]['Respuesta'];
+				var GuarMod=response[0]['GuarMod'];
+				
+				$("#MSJSubgrupo").html(Respuesta);
+            	$("#ModalMSJSubgrupo").modal("show");
+            	
+            	sessionStorage.setItem('GuarMod',GuarMod);
+              }
+              
+          }
+      });
+
+      return false;
+	}
+	
+}
+	
+</script>
+
+  <script>
+	
+$('#ModalMSJSubgrupo').on('hide.bs.modal', function (e) {
+		
+	var GuarMod = sessionStorage.getItem("GuarMod");
+	
+	sessionStorage.clear();	
+		
+	if(GuarMod =='Guardo')
+	{
+		window.open('formingresarmodificardatos.php', '_self');	
+	}
+	else if(GuarMod =='Modifico')
+	{
+		window.open('versubgrupos.php', '_self');	
 	}
 });
 	
